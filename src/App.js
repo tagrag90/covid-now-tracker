a17d43e529b6d6e6f675d4c8cb576fee8faffe2a
tagrag90 <gunjeepark21@gmail.com>
2020-07-22 21:36:17 +0900

    add dropdwon

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { MenuItem, FormControl, Select } from "@material-ui/core";
 import "./App.css";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+
+  // STATE = How to write a variable in React js.
+  // https://disease.sh/v3/covid-19/countries
+  // useEffect = Runs a piece of code
+  // based on a given condition
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }));
+
+          setCountries(countries);
+        });
+    };
+
+    getCountriesData();
+  }, []);
+
   return (
     <div className="app">
-      <h1>Lets build a Covid 19 Tracker</h1>
+      <div className="app__header">
+        <h1>Lets build a Covid 19 Tracker</h1>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" value="abc">
+            {/* Loop through all the countries and  */}
+            {countries.map((country) => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
 
       {/* Header */}
       {/* Title Select input dropdown field */}
