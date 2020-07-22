4c67c7fe18cf430602acc4b0bca8bc6f00b80ae4
tagrag90 <gunjeepark21@gmail.com>
2020-07-23 03:11:52 +0900

    make a column

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { MenuItem, FormControl, Select } from "@material-ui/core";
+import InfoBox from "./InfoBox";
+import Map from "./Map";
 import "./App.css";
 
 function App() {
   const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState("worldwide");
 
   // STATE = How to write a variable in React js.
   // https://disease.sh/v3/covid-19/countries
@@ -27,31 +30,45 @@ function App() {
     getCountriesData();
   }, []);
 
+  const onCountryChange = async (event) => {
+    const countryCode = event.target.value;
+    setCountry(countryCode);
+  };
+
   return (
     <div className="app">
-      <div className="app__header">
-        <h1>Lets build a Covid 19 Tracker</h1>
-        <FormControl className="app__dropdown">
-          <Select variant="outlined" value="abc">
-            {/* Loop through all the countries and  */}
-            {countries.map((country) => (
-              <MenuItem value={country.value}>{country.name}</MenuItem>
-            ))}
-          </Select>
-        </FormControl>
+      <div className="app__left">
+        <div className="app__header">
+          <h1>코로나 바이러스 현황</h1>
+          <FormControl className="app__dropdown">
+            <Select
+              variant="outlined"
+              onChange={onCountryChange}
+              value={country}
+            >
+              <MenuItem value="worldwide">worldwide</MenuItem>
+              {countries.map((country) => (
+                <MenuItem value={country.value}>{country.name}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </div>
+
+        <div className="app__stats">
+          <InfoBox title="코로나 확진" cases={123} total={2000} />
+
+          <InfoBox title="완 치" cases={1234} total={3000} />
+
+          <InfoBox title="사 망" cases={4322} total={4000} />
+        </div>
+
+        {/* Map */}
+        <Map />
       </div>
-
-      {/* Header */}
-      {/* Title Select input dropdown field */}
-
-      {/* Info Box */}
-      {/* Info Box */}
-      {/* Info Box */}
-
-      {/* Table */}
-      {/* Graph */}
-
-      {/* Map */}
+      <div className="app__right">
+        {/* Table */}
+        {/* Graph */}
+      </div>
     </div>
   );
 }
